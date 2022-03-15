20eef1028dbea9adfe90b5c2f3ccb8d6e1cd5f77
mattherix <mattherix@protonmail.com>
2022-03-15 12:51:03 +0100

    feat(chapter-3): add data type

--- projects/common_programming_concepts/src/main.rs
+++ projects/common_programming_concepts/src/main.rs
@@ -13,4 +13,40 @@ fn main() {
     // I can create constant but I *must* annotate them
     const QUADRUPLE_PI: f64 = 2.0 * TAU;
     // Btw happy PI day ! (look at the commit date)
+
+    // Data types
+    // We must annotate a variable if it can have multiple type
+    // i.e. with the parse function. Else the compiler will deduce it
+    let value: u32 = "42".parse().expect("Can't parse this value");
+    // Scalar type (single value): integer, float, bool or char
+    // 8-bit	i8	u8
+    // 16-bit	i16	u16
+    // 32-bit	i32	u32
+    // 64-bit	i64	u64
+    // 128-bit	i128	u128
+    // arch	isize	usize
+    // Can use _ in number, octal, char, ...
+    // Integer overflow: in debug -> panic in release wrap around to minimum
+    // Floating point, pretty usual
+    // /!\ if you divided 2 integers it will output only an integer, no f32/f64
+    // Just a bool: true or false
+    // Char, use utf8, can use emoji
+
+    // Compound type (multiple value in one): tuple or array
+    // Tuple, static different type.  can access with tuple.index
+    let tup = (500, 6.4, 1);
+    let (x, y, z) = tup;
+    println!("x={}, y={}, z ={} or {} {} {}", x, y, z, tup.0, tup.1, tup.2);
+
+    // Array, static multiple type
+    let a = [1, 2, 3, 4, 5];
+    let months: [&str; 12] = ["January", "February", "March", "April", "May", "June", "July",
+        "August", "September", "October", "November", "December"];
+    // Use using a for loop
+    for num in a {
+        println!("{num}");
+    }
+    // Or with the index
+    println!("{}", months[3]);
+    // Or use iterator (chapter 13) most common in the rust world
 }
